f2d7ad463a8d843de18887a5e7db63182f5c1ed2
Hameed <humiditii45@gmail.com>
2022-08-03 08:07:14 +0100

    -

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -11,6 +11,7 @@ export class BookService {
     private readonly configService: ConfigService
   ){}
 
+  
   async fetchBooks():Promise<{}[]>{
     try {
       const {data,status}: {data:[], status:number} = await this.httpService.axiosRef.get(this.configService.get('BASE_URL')+'/books')
@@ -56,25 +57,3 @@ export class BookService {
     }
   }
 }
-
-// {
-//   "url": "https://www.anapioficeandfire.com/api/books/1",
-//   "name": "A Game of Thrones",
-//   "isbn": "978-0553103540",
-//   "authors": [
-//     "George R. R. Martin"
-//   ],
-//   "numberOfPages": 694,
-//   "publisher": "Bantam Books",
-//   "country": "United States",
-//   "mediaType": "Hardcover",
-//   "released": "1996-08-01T00:00:00",
-//   "characters": [
-//     "https://www.anapioficeandfire.com/api/characters/2",
-//     ...
-//   ],
-//   "povCharacters": [
-//     "https://www.anapioficeandfire.com/api/characters/148",
-//     ...
-//   ]
-// }
